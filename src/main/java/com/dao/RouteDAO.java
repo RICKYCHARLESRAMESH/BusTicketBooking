84b32b50c3358ab4bdcbbd4bf3d3848f6b991f03
J <gopeeca.j@capgemini.com>
2024-12-18 17:31:02 +0530

    second

---
+++ src/main/java/com/dao/RouteDAO.java
@@ -0,0 +1,19 @@
+package com.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.model.Route;
+
+@Repository
+public interface RouteDAO extends JpaRepository<Route, Integer> {
+
+	List<Route> findByFromCity(String fromCity);
+
+	List<Route> findByToCity(String toCity);
+
+	List<Route> findByFromCityAndToCity(String fromCity, String toCity);
+	
+}
+
